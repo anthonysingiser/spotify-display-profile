62c5e030b69901742f9ca96de585a401d9bf70a3
Anthony <asingiser@gmail.com>
2023-09-23 12:25:19 -0400

    modify config to rewrite testing.html in page /

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: '/html/testing.html',
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
